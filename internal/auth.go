fc4b797fa90dffc0322862def836b99e5a1c2169
Vyacheslav <vyunner@gmail.com>
2026-02-13 19:16:03 +0500

    M  internal/auth.go

--- internal/auth.go
+++ internal/auth.go
@@ -62,12 +62,12 @@ func requestCode(c *gin.Context, db *sql.DB) {
 	code := "123456"
 
 	// PROD генерация (раскомментируй)
-	var err error
-	code, err = gen6Digits()
-	if err != nil {
-		Err(c, http.StatusInternalServerError, "CODE_GEN_ERROR", "code gen error")
-		return
-	}
+	//var err error
+	//code, err = gen6Digits()
+	//if err != nil {
+	//	Err(c, http.StatusInternalServerError, "CODE_GEN_ERROR", "code gen error")
+	//	return
+	//}
 
 	if _, err := db.Exec(`INSERT INTO sms_codes(phone, code) VALUES ($1,$2)`, phone, code); err != nil {
 		Err(c, http.StatusInternalServerError, "DB_ERROR", "db error")
