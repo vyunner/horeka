581113e3ab1d17866333bf5116716e565fbec3b6
Vyacheslav <vyunner@gmail.com>
2026-02-16 15:56:03 +0500

    M  migrations/000002_create_sms_codes.up.sql

--- migrations/000002_create_sms_codes.up.sql
+++ migrations/000002_create_sms_codes.up.sql
@@ -1,1 +1,15 @@
-DROP TABLE IF EXISTS sms_codes;
+CREATE TABLE sms_codes (
+                           id BIGSERIAL PRIMARY KEY,
+                           phone TEXT NOT NULL,
+                           code TEXT NOT NULL,
+                           created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+                           used_at TIMESTAMPTZ
+);
+
+CREATE INDEX idx_sms_codes_phone_active
+    ON sms_codes(phone, code)
+    WHERE used_at IS NULL;
+
+CREATE INDEX idx_sms_codes_used
+    ON sms_codes(used_at)
+    WHERE used_at IS NOT NULL;
